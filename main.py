7936f0d483a520c4e2863179041774a5ca200b44
miguel.reuter@ecl14.ec-lyon.fr <miguel.reuter@ecl14.ec-lyon.fr>
2018-03-20 11:02:57 +0100

    fundamental matrix calculation + epipolar lines display

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # encoding : utf-8
 
 import numpy as np
-
+import cv2
 
 def load_points(filename):		
 	with open(filename, 'r') as file:
@@ -88,12 +88,80 @@ def find_best_projection_matrix(pts2, pts3, k=(8, 12, 16), n=10):
 
 def compute_camera_center(M):
 	[Q, m4] = np.split(M, [3], axis=1)
-
 	C = - np.linalg.inv(Q) * np.matrix(m4)
-	print(C)
+	
 	return C
 
-if __name__ == "__main__":		
+
+def compute_fundamental_matrix(pts2_a, pts2_b):
+	n = pts2_a.shape[0]
+	A = []
+	
+	# construct A matrix for F computation
+	for i in range(n):
+		pt_ai = np.append(np.array(pts2_a[i, :]), ([[1]]))
+		pt_bi = np.append(np.array(pts2_b[i, :]), ([[1]]))
+		l = [pt_ai[k] * pt_bi[j] for j in range(3) for k in range(3)]
+		A.append(l)	
+	A = np.matrix(A)
+
+	# solving for F
+	[_, _, V] = np.linalg.svd(A)
+	F = V[-1, :].reshape((3, 3))
+	
+	# linear computation for F
+	[U, S, V] = np.linalg.svd(F)
+	S[-1] = 0
+	F = U * np.diag(S) * V
+
+	return F
+
+def compute_epipolar_lines(pts2_a, pts2_b, F):
+	n = pts2_a.shape[0]
+
+	picA_lines = np.zeros((n, 3))
+	picB_lines = np.zeros((n, 3))
+
+	for i in range(n):
+		pt_ai = np.matrix(np.append(np.array(pts2_a[i, :]), ([[1]])))
+		pt_bi = np.matrix(np.append(np.array(pts2_b[i, :]), ([[1]])))
+		
+		li_a = np.matrix(np.array(F * pt_bi.T)).T
+		picA_lines[i, :] = li_a / li_a[0, -1]
+		
+		li_b = np.matrix(np.array(F.T * pt_ai.T)).T
+		picB_lines[i, :] = li_b / li_b[0, -1]
+
+	return picA_lines, picB_lines
+	
+
+def draw_lines(pts_a, pts_b, l_a, l_b):
+	im_a = cv2.imread("input/pic_a.jpg")
+	im_b = cv2.imread("input/pic_b.jpg")
+	
+	# image a
+	x_min = 0
+	x_max = im_b.shape[1]
+	for i in range(pts_b.shape[0]):		
+		y_min = int(pts_a[i, 1] + (x_min - pts_a[i, 0]) * (l_a[i, 0] / l_a[i, 1]))
+		y_max = int(pts_a[i, 1] + (x_max - pts_a[i, 0]) * (l_a[i, 0] / l_a[i, 1]))
+		cv2.line(im_a, (x_min, y_min), (x_max, y_max), (0,0,255), 1)
+
+	# image b
+	x_min = 0
+	x_max = im_b.shape[1]
+	for i in range(pts_a.shape[0]):		
+		y_min = int(pts_b[i, 1] + (x_min - pts_b[i, 0]) * (l_b[i, 0] / l_b[i, 1]))
+		y_max = int(pts_b[i, 1] + (x_max - pts_b[i, 0]) * (l_b[i, 0] / l_b[i, 1]))
+		cv2.line(im_b, (x_min, y_min), (x_max, y_max), (0,0,255), 1)
+
+	cv2.imshow("image a", im_a)
+	cv2.imshow("image b", im_b)
+	cv2.waitKey(0)
+
+
+
+if __name__ == "__main__":
 	# M_norm_A	
 	pts_2d = load_points("input/pts2d-norm-pic_a.txt")
 	pts_3d = load_points("input/pts3d-norm.txt")
@@ -105,17 +173,22 @@ if __name__ == "__main__":
 	
 	
 	# M_B
-	pts_2d = load_points("input/pts2d-pic_b.txt")
-	pts_3d = load_points("input/pts3d.txt")
+	#pts_2d = load_points("input/pts2d-pic_b.txt")
+	#pts_3d = load_points("input/pts3d.txt")
 	
-	[r_mat, m] = find_best_projection_matrix(pts_2d, pts_3d)
-	r = get_residual_error(pts_2d, pts_3d, m)
-	
-
+	#[r_mat, m] = find_best_projection_matrix(pts_2d, pts_3d)
+	#r = get_residual_error(pts_2d, pts_3d, m)
 	#compute_camera_center(m)
 	#print("residual error : ", r)
 
+	# fundamental matrix
+	pts_2d_a = load_points("input/pts2d-pic_a.txt")
+	pts_2d_b = load_points("input/pts2d-pic_b.txt")
+	F = compute_fundamental_matrix(pts_2d_a, pts_2d_b)
 
+	[lines_a, lines_b] = compute_epipolar_lines(pts_2d_a, pts_2d_b, F)
+
+	draw_lines(pts_2d_a, pts_2d_b, lines_a, lines_b)
 
 
 
